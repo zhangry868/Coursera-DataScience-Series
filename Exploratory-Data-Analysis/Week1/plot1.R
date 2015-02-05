7fcd9e310dda96c3797e81354ce3f39bbbf2dbaf
zhangry868 <zhangry868@126.com>
2015-02-06 01:00:51 +0800

    2015.2.6

---
+++ Exploratory-Data-Analysis/Week1/plot1.R
@@ -0,0 +1,13 @@
+## First check whether it has the file in the current dir.
+
+source("load_data.R")
+png(filename = "plot1.png", 
+    width = 480, height = 480, 
+    units = "px", bg = "transparent")
+hist(Global_active_power, 
+     col = "red", 
+     main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)",
+     breaks = 12, ylim = c(0, 1200))
+dev.off()
+
---
+++ Exploratory-Data-Analysis/Week1/plot2.R
@@ -0,0 +1,11 @@
+## First check whether it has the file in the current dir.
+
+source("load_data.R")
+png(filename = "plot2.png", 
+     width = 480, height = 480,
+     units = "px", bg = "transparent")
+plot(DateTime, Global_active_power, 
+     type = "l",
+     xlab = "",
+     ylab = "Global Active Power (kilowatts)")
+dev.off()
